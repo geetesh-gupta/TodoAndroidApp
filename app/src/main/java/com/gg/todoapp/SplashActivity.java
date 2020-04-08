e08e81377e10a16df57520a3c15f46bba9e56e71
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-04-08 14:35:33 +0530

    Setup Login Check in SplashActivity

--- app/src/main/java/com/gg/todoapp/SplashActivity.java
+++ app/src/main/java/com/gg/todoapp/SplashActivity.java
@@ -2,13 +2,35 @@ package com.gg.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setupPreferences();
+        checkLoginStatus();
+    }
+
+
+    private void checkLoginStatus(){
+        boolean isLoggedIn = sharedPreferences.getBoolean(PrefConstant.IS_LOGGED_IN, false);
+        if (isLoggedIn){
+            Intent intent = new Intent(SplashActivity.this, NotesActivity.class);
+            startActivity(intent);
+        }else {
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
+    private void setupPreferences(){
+        sharedPreferences = getSharedPreferences(PrefConstant.SHARED_PREFERENCE_NAME, MODE_PRIVATE);
     }
 }
